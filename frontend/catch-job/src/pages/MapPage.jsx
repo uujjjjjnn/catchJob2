6f051874e934f1463e27a04f1238717623ac378c
choigukhee <cghee0718@naver.com>
2023-08-18 15:42:45 +0900

    마커 삭제

--- frontend/catch-job/src/pages/MapPage.jsx
+++ frontend/catch-job/src/pages/MapPage.jsx
@@ -1,30 +1,49 @@
 import React from 'react';
 import { useState, useEffect, useRef } from "react"
 import styles from "../assets/css/Map.module.css";
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
+const containerStyle = {
+  width: '1000px',
+  height: '800px',
+};
+
+const center = {
+    lat: 37.569227, lng: 126.9777256
+};
 
 const MapPage = () => {
     
-    const ref = useRef();
-    const API_KEY = "AIzaSyDyCBaHuD_xiJCzf_EH1Q_0R5WRaiA0LiM";
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: "AIzaSyDyCBaHuD_xiJCzf_EH1Q_0R5WRaiA0LiM"
+      })
+    
+      const [map, setMap] = React.useState(null)
+    
+      const onLoad = React.useCallback(function callback(map) {
+        new window.google.maps.LatLngBounds(center);
 
-useEffect(()=>{
-    const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&libraries=places`;
-    script.async = true;
-    document.body.appendChild(script);
-
-    script.onload = () => {
-        new window.google.maps.Map(ref.current, {
-            center: { lat: 37.569227, lng: 126.9777256 },
-            zoom: 16,
+            map.setOptions({ styles: [
+            {featureType:'poi',stylers:[{visibility:'off'}]},],
         });
-    }         
-}, [])
-
-    return (
-        <div className={`${styles.mapLooking}`} ref={ref} id="map"></div>
-    )
-};
-
+    
+        setMap(map)
+      }, [])
+    
+      const onUnmount = React.useCallback(function callback(map) {
+        setMap(null)
+      }, [])
+    
+      return isLoaded ? (
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={17}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+          >
+          </GoogleMap>
+      ) : <></>
+    }
 export default MapPage;
